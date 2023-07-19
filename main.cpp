74dab70d55b569e89e56c22e185d7347638f831a
Maxim Shtuchka <maxim1000@gmail.com>
2023-07-19 22:11:24 +0300

    placed a 3D scene onto the window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QOpenGLWidget>
+#include <QVTKOpenGLNativeWidget.h>
+class TMainWindow:public QMainWindow
+{
+public:
+	TMainWindow();
+private:
+	QVTKOpenGLNativeWidget *Scene;
+};
+TMainWindow::TMainWindow()
+	:QMainWindow(nullptr)
+{
+	setWindowTitle("Percolation");
+	resize(800,600);
+	Scene=new QVTKOpenGLNativeWidget(this);
+	setCentralWidget(Scene);
+}
 int main(int argc,char *argv[])
 {
 	QApplication app(argc,argv);
-	QMainWindow window(nullptr);
+	TMainWindow window;
 	window.show();
 	return app.exec();
 }
